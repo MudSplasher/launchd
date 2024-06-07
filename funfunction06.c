83a54f9e19915a79f765387674d3ddd82c7d830e
MudSplasher <150399924+MudSplasher@users.noreply.github.com>
2024-06-06 21:01:59 -0400

    Create funfunction06.c

---
+++ funfunction06.c
@@ -0,0 +1,19 @@
+void FUN_10000c014(undefined8 param_1,undefined8 param_2,undefined8 param_3,undefined8 param_4)     // defiend when system is ran
+
+{
+  undefined *local_40;
+  undefined8 local_38;
+  code *local_30;
+  undefined *puStack_28;
+  undefined8 local_20;
+  undefined8 uStack_18;
+  
+  local_40 = PTR___NSConcreteStackBlock_100075058;
+  local_38 = 0x40000000;
+  local_30 = FUN_10000c084;
+  puStack_28 = &DAT_100076800;
+  local_20 = param_2;
+  uStack_18 = param_4;
+  _xpc_array_apply(&local_30,param_3,&local_40);
+  return;
+}
